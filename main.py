b1f159a155d56dcedf6b71064ff3a006907932b9
ManinderSingh4000 <saabm314@gmail.com>
2026-04-09 12:43:18 +0530

    Data Changed

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from fastapi import FastAPI
 import numpy as np
+import random
 
 app = FastAPI()
 
-arr = np.array([10,20,40,30,70])
+data = {
+    "Marks" : random.sample(range(100),10) 
+}
 
 @app.get("/")
 def index():
@@ -11,5 +14,5 @@ def index():
 
 @app.get("/check")
 def show_array():
-    return {"Array" : arr }
+    return data
     
